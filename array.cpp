d19be4078c5f40e98792e96f9307ad8ba948a319
tashwitab <142425346+tashwitab@users.noreply.github.com>
2023-08-18 21:19:50 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n[10];
+    int i, j;
+
+    for (i = 0; i < 10; i++)
+    {
+        cout << "Enter value of n[" << i << "]" << endl;
+        cin >> n[i];
+    }
+    for (j = 0; j < 10; j++)
+    {
+        cout << "n[" << j << "]=" << n[j] << endl;
+    }
+    return 0;
+}
